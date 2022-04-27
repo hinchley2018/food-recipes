8bc0a60752d17726a90037e7a4875c37cc42a492
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2022-04-26 19:30:34 -0500

    finish recipe website

--- index.js
+++ index.js
@@ -40,19 +40,19 @@ let availableRecipes = [
 //This is what each recipe should look like
 /*
 <div id="taco" class="recipe">
-        <h3>Taco Recipe</h3>
-        <h4>Ingredients</h4>
-        <ul>
-            <li>Ground Turkey</li>
-            <li>tortillas</li>
-        </ul>
-        Duration: 15min
-        <h4>Instructions</h4>
-        <ol>
-            <li>Cook ingredients</li>
-            <li>Assemble taco</li>
-        </ol>
-    </div>
+    <h3>Taco Recipe</h3>
+    <h4>Ingredients</h4>
+    <ul>
+        <li>Ground Turkey</li>
+        <li>tortillas</li>
+    </ul>
+    Duration: 15min
+    <h4>Instructions</h4>
+    <ol>
+        <li>Cook ingredients</li>
+        <li>Assemble taco</li>
+    </ol>
+</div>
 */
 
 //we have a recipe list we want to go through lets use a loop (aka merry go round)
@@ -64,15 +64,72 @@ for(let index = 0; index < availableRecipes.length; index++){
     //display each of our recipes on the website
     //make it look liek the div above 
 
+    //create a div for each recipe markup / html stuff
+    let recipeDiv = document.createElement("div")
+    recipeDiv.setAttribute("id", recipeObject.id)
+    recipeDiv.setAttribute("class", "recipe")
+
     //start simple create h3 on the document.body
     let recipeTitle = document.createElement("h3")
     recipeTitle.textContent = recipeObject.name
+    recipeDiv.append(recipeTitle)
+
+    //ingredients h4
+    let ingredientsH4 = document.createElement("h4")
+    ingredientsH4.textContent = "Ingredients"
+    recipeDiv.append(ingredientsH4)
+
+    //ingredients ul
+    let ingredientsUl = document.createElement("ul")
+    recipeDiv.append(ingredientsUl)
+
+    //add ingredients to unorded list
+    for (let ingredientIndex = 0; ingredientIndex < recipeObject.ingredients.length; ingredientIndex++) {
+        let ingredient = recipeObject.ingredients[ingredientIndex]
+        console.log("ingredientIndex", ingredientIndex, ingredient)
+        let ingredientListItem = document.createElement("li")
+        ingredientListItem.textContent = ingredient
+        ingredientsUl.append(ingredientListItem)
+    }
+
+    //instructions h4
+    let instructionsH4 = document.createElement("h4")//create element
+    instructionsH4.textContent = "Instructions"//manipulate
+    recipeDiv.append(instructionsH4)//append
+
+    //instructions ol
+    let instructionsOl = document.createElement("ol")
+    recipeDiv.append(instructionsOl)
+
+    //add instructions to ordered list
+    for (let instructionIndex = 0; instructionIndex < recipeObject.instructions.length; instructionIndex++) {
+        let instruction = recipeObject.instructions[instructionIndex]
+        console.log("instructionIndex", instructionIndex, instruction)
+        let instructionListItem = document.createElement("li")
+        instructionListItem.textContent = instruction
+        instructionsOl.append(instructionListItem)
+    }
 
     //trivial way but not great -> gets messy
     // document.body.append(recipeTitle)
 
-    let recipeContainer = document.querySelector("#recipe-container")
-    console.log(recipeContainer)
+    let recipesContainer = document.querySelector("#recipes-container")
+    console.log(recipesContainer)
 
-    recipeContainer.append(recipeTitle)
+    //append all the elements to the container 
+    recipesContainer.append(recipeDiv)
 }
+
+
+
+//selecting by id
+let footer = document.getElementById("footer")
+console.log(footer)
+//essentially the same thing
+//let footer = document.querySelector("#footer")
+
+//now set the text of footer
+footer.textContent = "Copyright CH 2022"
+
+//now change the style of foooter
+footer.style.color = "blue"
